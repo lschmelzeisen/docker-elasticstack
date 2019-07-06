f208b633c7098e7edae8ca64eb9f65f5752304dc
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2019-07-06 22:49:02 +0200

    Store generated encryption keys in kibana keystore.
    Should be more secure and better design than just appending them to the
    config file.

--- containers/setup_kibana/entrypoint.sh
+++ containers/setup_kibana/entrypoint.sh
@@ -6,12 +6,6 @@ cp -r config/. /kibana_config
 cp -r data/. /kibana_data
 cp -r /pwd/config.skel/kibana/. /kibana_config
 
-cat <<YML >>/kibana_config/kibana.yml
-xpack.security.encryptionKey: "$(tr -dc [:alnum:] < /dev/urandom | head -c 32)"
-xpack.encrypted_saved_objects.encryptionKey: "$(tr -dc [:alnum:] < /dev/urandom | head -c 32)"
-xpack.reporting.encryptionKey: "$(tr -dc [:alnum:] < /dev/urandom | head -c 32)"
-YML
-
 rm -r config data
 ln -s /kibana_config config
 ln -s /kibana_data data
@@ -23,6 +17,9 @@ chown -R 1000 /kibana_data
 su kibana <<SU
 bin/kibana-keystore create
 bin/kibana-keystore add "elasticsearch.password" -x < /passwords/kibana
+tr -dc [:alnum:] < /dev/urandom | head -c 32 | bin/kibana-keystore add "xpack.security.encryptionKey" -x
+tr -dc [:alnum:] < /dev/urandom | head -c 32 | bin/kibana-keystore add "xpack.encrypted_saved_objects.encryptionKey" -x
+tr -dc [:alnum:] < /dev/urandom | head -c 32 | bin/kibana-keystore add "xpack.reporting.encryptionKey" -x
 SU
 
 unlink config/certs
